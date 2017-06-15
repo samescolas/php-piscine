a9e5d5e670b7d817f64e8bb78389600fafe5a4dd
Sam Escolas <samescolas@gmail.com>
2017-06-15 04:38:54 -0400

    Adding a Die class

---
+++ day08/Die.class.php
@@ -0,0 +1,42 @@
+<?php
+
+class Dice {
+	private $num_sides;
+	private $value;
+	static $verbose = False;
+
+	public function __construct($args) {
+		if (isset($args['num_sides']))
+			$this->num_sides = $args['num_sides'];
+		else
+			$this->num_sides = 6;
+		if (self::$verbose)
+			echo "Spun up a " . $this->num_sides . " sided die." . PHP_EOL;
+	}
+
+	public function rollDie() {
+		$this->value = rand(1, $this->num_sides);
+		return $this->value;
+	}
+
+	public function __toString() {
+		return "D" . $this->num_sides;
+	}
+
+	public function __destruct() {
+		if (self::$verbose)
+			echo "$this destructed." . PHP_EOL;
+	}
+}
+
+Dice::$verbose = True;
+$dice = array (
+	new Dice( array('num_sides' => 8) ),
+	new Dice(array())
+);
+
+echo 'Rolling the dice!' . PHP_EOL;
+foreach ($dice as $d)
+	echo "Rolled a " . $d->rollDie() . PHP_EOL;
+
+?>
